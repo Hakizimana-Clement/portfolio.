26c9a13c66840c461b214e5f8e02a432bf89dde6
Hakizimana Clement <h.clementoert@gmail.com>
2024-02-26 17:21:28 +0200

    Add swiper js on project section

--- src/slider-project-function.js
+++ src/slider-project-function.js
@@ -1,11 +1,14 @@
 const projectTitle = document.querySelector(
   ".projects-container__tex-subtitle"
 );
-
 const projectImg = document.querySelector(".projects-container__image-img");
 const projectParagraphs = document.querySelector(
   ".projects-container__tex-description"
 );
+
+// const allProjectBtn = document.querySelectorAll(
+//   ".projects-container__text-next-btn-container"
+// );
 
 // project 1
 const project1 = document.querySelector(".project-1");
@@ -22,9 +25,9 @@ project1.addEventListener("click", () => {
 const project2 = document.querySelector(".project-2");
 project2.addEventListener("click", () => {
   console.log("clicked");
-  projectTitle.textContent = "Music App ";
-  projectImg.src =
-    "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  projectTitle.textContent = "Dj App ";
+  // projectImg.src = "../images/project-3-music-app.jpg";
+  projectImg.src = "../images/project-1.svg";
   projectParagraphs.textContent =
     "  Lorem ipsum dolor sit amet consectetur adipisicing eli  Lorem ipsum dolor sit amet consectetur adipisicing elitLorem ipsum dolor sit amet consectetur adipisicing elitLorem ipsum dolor sit amet consectetur adipisicing elitLorem ipsum dolor sit amet consectetur adipisicing elittLorem ipsum dolor sit amet consectetur adipisicing elit... ";
   // button
@@ -32,7 +35,7 @@ project2.addEventListener("click", () => {
   // text
   // one buton
   document.querySelector(".one-num").style.color = "#fff";
-  document.querySelector(".one-num-btn").style.background = "#fff";
+  document.querySelector(".one-num-btn").style.backgroundColor = "#fff";
 
   document.querySelector(".two-num").style.color = "#5161f1";
 });
@@ -42,8 +45,7 @@ const project3 = document.querySelector(".project-3");
 project3.addEventListener("click", () => {
   console.log("clicked project 3");
   projectTitle.textContent = "Book keep app";
-  projectImg.src =
-    "https://images.unsplash.com/photo-1541963463532-d68292c34b19?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  projectImg.src = "../images/project-2-book-app.jpg";
   projectParagraphs.textContent =
     " Lorem ipsum dolor sit amet consectetur adipisicing eli  Lorem ipsum dolor sit amet consectetur adipisicing elitLorem ipsum dolor sit amet consectetur adipisicing elitLorem ipsum dolor sit amet consectetur adipisicing elitLorem iadipisicing elittLorem ipsum dolor sit amet consectetur adipisicing elit... ";
 });
@@ -53,8 +55,8 @@ const project4 = document.querySelector(".project-4");
 project4.addEventListener("click", () => {
   console.log("clicked project 4");
   projectTitle.textContent = "Snake game";
-  projectImg.src =
-    "https://images.unsplash.com/photo-1541963463532-d68292c34b19?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+  projectImg.src = "../images/project-4.-snake-game.jpg";
   projectParagraphs.textContent =
     " This snake Lorem ipsum dolor sit amet consectetur adipisicing eli  Lorem ipsum dolor sit amet consectetur adipisicing elitLorem ipsum dolor sit amet consectetur adipisicing elitLorem ipsum dolor sit amet consectetur adipisicing elitLorem iadipisicing elittLorem ipsum dolor sit amet consectetur adipisicing elit... ";
 });
