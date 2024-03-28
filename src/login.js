880cdaf024bc92d625242a4162586e4d2ef48ad7
Hakizimana Clement <h.clementoert@gmail.com>
2024-03-29 00:00:56 +0200

    Add: frontedn validation on signup page

--- src/login.js
+++ src/login.js
@@ -1,5 +1,20 @@
 const loginFormEl = document.querySelector(".signup-container__form-form");
+const showLoaderContainer = document.querySelector(".loader-container");
 
+const showFormErros = (error) => {
+  // email
+  document.querySelector("#email-error").textContent = error.emailError || "";
+  document.querySelector("#password-error").textContent =
+    error.passwordError || "";
+};
+
+const showLoader = () => {
+  showLoaderContainer.style.display = "flex";
+};
+
+const hideShowLoader = () => {
+  showLoaderContainer.style.display = "none";
+};
 // error form
 const formError = {
   emailError: null,
@@ -14,70 +29,77 @@ const showFormErrors = (error) => {
 };
 
 // login form
-loginFormEl.addEventListener("submit", (e) => {
-  console.log("clicked");
+loginFormEl.addEventListener("submit", async (e) => {
+  // console.log("clicked");
   e.preventDefault();
 
-  // state
-  let hasErrors = false;
+  const userDataToLogin = {
+    email: e.target.elements.email.value,
+    password: e.target.elements.password.value,
+  };
+  console.log(userDataToLogin);
+  try {
+    const response = await fetch("http://localhost:4000/api/v1/users/login", {
+      method: "POST",
+      body: JSON.stringify(userDataToLogin),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
+    if (!response.ok) {
+      const error = json.error || "";
+      const errors = {
+        emailError: error.includes("email") ? error : "",
+        passwordError: error.includes("password") ? error : "",
+      };
+      showFormErros(errors);
+      hideShowLoader();
+    }
 
-  // validate email
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailPattern.test(e.target.elements.email.value)) {
-    formError.emailError = "Invalid email address.";
-    hasErrors = true;
-  } else {
-    formError.emailError = null;
+    if (response.status === 201) {
+      showLoader();
+      // console.log("new user", json);
+      // signupFormEl.reset();
+      window.location.href = "signin.html";
+    }
+    console.log(json);
+  } catch (error) {
+    console.log(error);
   }
 
-  // // validate password
-  if (e.target.elements.password.value.length === 0) {
-    formError.passwordError = "Password should not be empty";
-    hasErrors = true;
-  } else if (e.target.elements.password.value.length <= 8) {
-    formError.passwordError = "Password should not be less than 8 character";
-    hasErrors = true;
-  } else {
-    formError.passwordError = null;
-  }
-  // else if (e.target.elements.password.value.length > 8) {
-  //   formError.passwordError = "Password should be less than 8 character";
+  // // state
+  // let hasErrors = false;
+
+  // // validate email
+  // const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // if (!emailPattern.test(e.target.elements.email.value)) {
+  //   formError.emailError = "Invalid email address.";
   //   hasErrors = true;
+  // } else {
+  //   formError.emailError = null;
   // }
 
-  // Show form errors
-  showFormErrors(formError);
+  // // // validate password
+  // if (e.target.elements.password.value.length === 0) {
+  //   formError.passwordError = "Password should not be empty";
+  //   hasErrors = true;
+  // } else if (e.target.elements.password.value.length <= 8) {
+  //   formError.passwordError = "Password should not be less than 8 character";
+  //   hasErrors = true;
+  // } else {
+  //   formError.passwordError = null;
+  // }
 
-  if (!hasErrors) {
-    // Form submission logic here
-    console.log("Form submitted successfully!");
-    e.target.reset(); // Reset the form after successful submission
-    location.assign(
-      "https://hakizimana-clement.github.io/my-brand-Clement-Hakizimana/pages/admin-panel.html"
-    );
-  }
+  // // Show form errors
+  // showFormErrors(formError);
+
+  // if (!hasErrors) {
+  //   // Form submission logic here
+  //   console.log("Form submitted successfully!");
+  //   e.target.reset(); // Reset the form after successful submission
+  //   location.assign(
+  //     "https://hakizimana-clement.github.io/my-brand-Clement-Hakizimana/pages/admin-panel.html"
+  //   );
+  // }
 });
-
-// const username = document.querySelector("#email");
-// const password = document.querySelector("#password");
-
-// const form = document.querySelector(".signup-container__form-form");
-// const errorEl = document.querySelector("#error");
-
-// form.addEventListener("submit", (e) => {
-//   let messages = [];
-//   // email check
-//   if (username.value === "" || username.value == null) {
-//     messages.push("Email is required");
-//   }
-
-//   // password
-//   if (password.value === "" || password.value == null) {
-//     messages.push("Password is required");
-//   }
-
-//   if (messages.length > 0) {
-//     e.preventDefault();
-//     errorEl.innerHTML = messages.join(", ");
-//   }
-// });
--- src/signup-form.js
+++ src/signup-form.js
@@ -2,6 +2,22 @@ const signupFormEl = document.querySelector(".signup-container__form-form");
 const showLoaderContainer = document.querySelector(".loader-container");
 
 // display error below input
+const showLoader = () => {
+  showLoaderContainer.style.display = "flex";
+};
+
+const hideShowLoader = () => {
+  showLoaderContainer.style.display = "none";
+};
+
+let formErrors = {
+  nameError: null,
+  emailError: null,
+  passwordError: null,
+  confirmPasswordError: null,
+};
+
+// display error below input
 const showFormErros = (error) => {
   // email
   document.querySelector("#name-error").textContent = error.nameError || "";
@@ -12,77 +28,180 @@ const showFormErros = (error) => {
     error.confirmPasswordError || "";
 };
 
-const showLoader = () => {
-  showLoaderContainer.style.display = "flex";
-};
-
-const hideShowLoader = () => {
-  showLoaderContainer.style.display = "none";
-};
-
+// sign form
 signupFormEl.addEventListener("submit", async (e) => {
+  console.log("clicked");
   e.preventDefault();
+
+  // state
+  let hasErrors = false;
+
+  // Validate full name
+  if (e.target.elements.name.value.length === 0) {
+    formErrors.nameError = "full name should not be empty.";
+    hasErrors = true;
+  } else if (e.target.elements.name.value.length < 5) {
+    formErrors.nameError = "full name should not be less than 5 character.";
+    hasErrors = true;
+  } else {
+    formErrors.nameError = null;
+  }
+
+  // validate email
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(e.target.elements.email.value)) {
+    formErrors.emailError = "Invalid email address.";
+    hasErrors = true;
+  } else {
+    formErrors.emailError = null;
+  }
+
+  // validate password
+  if (e.target.elements.password.value.length === 0) {
+    formErrors.passwordError = "password  should not be empty.";
+    hasErrors = true;
+  } else if (e.target.elements.password.value.length < 8) {
+    formErrors.passwordError = "password should not be less than 8 character.";
+    hasErrors = true;
+  } else if (e.target.elements.password.value.length >= 15) {
+    formErrors.passwordError =
+      "password  should not greater than 15 character.";
+    hasErrors = true;
+  } else {
+    formErrors.passwordError = null;
+  }
+
+  // validate confirm password
+  if (e.target.elements.confirmPassword.value.length === 0) {
+    formErrors.confirmPasswordError = "confirm password  should not be empty.";
+    hasErrors = true;
+  } else if (
+    e.target.elements.confirmPassword.value.length !==
+    e.target.elements.password.value.length
+  ) {
+    formErrors.confirmPasswordError = "Re-enter password again.";
+    hasErrors = true;
+  } else {
+    formErrors.confirmPasswordError = null;
+  }
+
+  showFormErros(formErrors);
+
   const userDataToSignup = {
     name: e.target.elements.name.value,
     email: e.target.elements.email.value,
     password: e.target.elements.password.value,
     "confirm password": e.target.elements.confirmPassword.value,
   };
-  try {
-    const response = await fetch("http://localhost:4000/api/v1/users/signup", {
-      method: "POST",
-      body: JSON.stringify(userDataToSignup),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const json = await response.json();
-    if (!response.ok) {
-      const error = json.error || "";
+  if (!hasErrors) {
+    // Form submission logic here
+    console.log("login successfully!");
+    e.target.reset();
 
-      let nameError = "";
-      let emailError = "";
-      let passwordError = "";
-      let confirmPasswordError = "";
-
-      // Check for specific error messages related to each field
-      if (error.includes("name")) {
-        nameError = error;
-      }
-      if (error.includes("email")) {
-        emailError = error;
-      }
-      if (error.includes("Email already in use")) {
-        confirmPasswordError = error;
-      } else if (error.includes("confirm password is required")) {
-        confirmPasswordError = error;
-      } else if (error.includes("Password do not match")) {
-        confirmPasswordError = error;
-      } else if (error.includes("password")) {
-        passwordError = error;
-      }
-
-      const errors = {
-        nameError,
-        emailError,
-        passwordError,
-        confirmPasswordError,
-      };
-      showFormErros(errors);
-      hideShowLoader();
-    }
-
-    if (response.status === 201) {
+    try {
       showLoader();
-      // console.log("new user", json);
-      // signupFormEl.reset();
-      window.location.href = "signin.html";
+      const response = await fetch(
+        "http://localhost:4000/api/v1/users/signup",
+        {
+          method: "POST",
+          body: JSON.stringify(userDataToSignup),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const json = await response.json();
+
+      if (!response.ok) {
+        console.log(json);
+      }
+      if (response.status === 201) {
+        showLoader();
+        console.log("new user", json);
+        window.location.href = "signin.html";
+      }
+    } catch (error) {
+      console.log(error);
     }
-    // console.log(json);
-  } catch (error) {
-    console.log(error);
   }
 });
+
+// const showFormErros = (error) => {
+//   // email
+//   document.querySelector("#name-error").textContent = error.nameError || "";
+//   document.querySelector("#email-error").textContent = error.emailError || "";
+//   document.querySelector("#password-error").textContent =
+//     error.passwordError || "";
+//   document.querySelector("#confirm-password-error").textContent =
+//     error.confirmPasswordError || "";
+// };
+
+// signupFormEl.addEventListener("submit", async (e) => {
+//   e.preventDefault();
+//   const userDataToSignup = {
+//     name: e.target.elements.name.value,
+//     email: e.target.elements.email.value,
+//     password: e.target.elements.password.value,
+//     "confirm password": e.target.elements.confirmPassword.value,
+//   };
+//   try {
+//     const response = await fetch(
+//       "https://mybrand-be-j4ci.onrender.com/api/v1/users/signup",
+//       {
+//         method: "POST",
+//         body: JSON.stringify(userDataToSignup),
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     );
+//     const json = await response.json();
+//     if (!response.ok) {
+//       const error = json.error || "";
+
+//       let nameError = "";
+//       let emailError = "";
+//       let passwordError = "";
+//       let confirmPasswordError = "";
+
+//       // Check for specific error messages related to each field
+//       if (error.includes("name")) {
+//         nameError = error;
+//       }
+//       if (error.includes("email")) {
+//         emailError = error;
+//       }
+//       if (error.includes("Email already in use")) {
+//         confirmPasswordError = error;
+//       } else if (error.includes("confirm password is required")) {
+//         confirmPasswordError = error;
+//       } else if (error.includes("Password do not match")) {
+//         confirmPasswordError = error;
+//       } else if (error.includes("password")) {
+//         passwordError = error;
+//       }
+
+//       const errors = {
+//         nameError,
+//         emailError,
+//         passwordError,
+//         confirmPasswordError,
+//       };
+//       showFormErros(errors);
+//       hideShowLoader();
+//     }
+
+//     if (response.status === 201) {
+//       showLoader();
+//       // console.log("new user", json);
+//       // signupFormEl.reset();
+//       window.location.href = "signin.html";
+//     }
+//     // console.log(json);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 // sign form
 // signupFormEl.addEventListener("submit", (e) => {
@@ -162,3 +281,6 @@ signupFormEl.addEventListener("submit", async (e) => {
 //       : "",
 //   };
 //   showFormErros(errors);
+// const signupFormEl = document.querySelector(".signup-container__form-form");
+
+// errors
