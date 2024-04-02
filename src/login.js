dd12bf9dee7713984f4d3ee3eb3d305d014dbc56
Hakizimana Clement <h.clementoert@gmail.com>
2024-04-02 13:29:30 +0200

    Add: https api from Render in login page.

--- src/login.js
+++ src/login.js
@@ -1,13 +1,40 @@
 const loginFormEl = document.querySelector(".signup-container__form-form");
-const showLoaderContainer = document.querySelector(".loader-container");
 const errorIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"
 class="toast-icons"><path fill="#3498db" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m1 15h-2v-6h2zm0-8h-2V7h2z"/></svg>`;
+
+// ************************** LOADER **************************
+const loaderContainer = document.querySelector(".loader-container");
 
 function decodeJwt(token) {
   const base64Payload = token.split(".")[1];
   const payloadBuffer = window.atob(base64Payload, "base64");
   return JSON.parse(payloadBuffer.toString());
 }
+
+// remove user token for fresh login
+localStorage.removeItem("userToken");
+
+const showLoaderr = () => {
+  loaderContainer.style.display = "flex";
+};
+
+const hideLoaderr = () => {
+  loaderContainer.style.display = "none";
+};
+// // ************************** CHECK USER TOKEN **************************
+// const token = localStorage.getItem("userToken");
+// if (!token) {
+//   console.log("Token is missing.");
+//   // location.assign("../signin.html");
+// } else {
+//   const decodedPayload = decodedJwt(token);
+//   if (decodedPayload)
+//     if (decodedPayload.role !== "user") {
+//       console.log("User is not an login. Redirecting to login page.");
+//       // location.assign("../signin.html");
+//     }
+// }
+// console.log(("token", token));
 
 // error form
 const formError = {
@@ -70,6 +97,7 @@ loginFormEl.addEventListener("submit", async (e) => {
 
   if (!hasErrors) {
     try {
+      showLoaderr();
       // user data to login
       const loginUserData = {
         email: e.target.elements.email.value,
@@ -77,25 +105,30 @@ loginFormEl.addEventListener("submit", async (e) => {
       };
 
       // send to api for login
-      const response = await fetch("http://localhost:4000/api/v1/users/login", {
-        method: "POST",
-        body: JSON.stringify(loginUserData),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      // const response = await fetch("http://localhost:4000/api/v1/users/login", {
+      const response = await fetch(
+        "https://mybrand-be-j4ci.onrender.com/api/v1/users/login",
+        {
+          method: "POST",
+          body: JSON.stringify(loginUserData),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       const json = await response.json();
 
       // no response
       if (!response.ok) {
+        hideLoaderr();
         console.log(json);
         createToast(error, errorIcon, "Error", json.error);
       }
 
       if (response.ok) {
         // show loader when user wait to login
-        showLoader();
+        // showLoader();
 
         const token = json.token;
         // decoded toke
@@ -112,6 +145,8 @@ loginFormEl.addEventListener("submit", async (e) => {
       }
     } catch (error) {
       console.log(error);
+    } finally {
+      hideLoaderr();
     }
   }
 
